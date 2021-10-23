77d32e017febf6a028359d1d44aa6ddfb7c4e605
MuhammadMooazam <muhammadmoazam29@gmail.com>
2021-10-24 04:04:15 +0500

    add files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mooazam',
+    version='0.2',
+    description="This is Mooazam's package",
+    long_description="This is Mooazam's package",
+    author="Syed Muhammad Mooazam",
+    author_email="muhammadmoazam29@gmail.com",
+    packages=["mooazam"],
+    install_requires=[]
+)
